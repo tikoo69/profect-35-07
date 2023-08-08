ef095fbe15525ce1f4d746dc8db844b46ea66b97
PongPorn Sarad-iam <tikoouou@gmail.com>
2023-08-08 13:12:42 +0700

    first commit

---
+++ resources/views/layouts/masterauthen.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('blackend/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('blackend/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{asset('blackend/assets/css/style.css')}}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{asset('blackend/assets/images/favicon.png')}}" />
+  </head>
+  <body>
+
+    @yield('content')
+
+
+  
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{asset('blackend/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{asset('blackend/assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('blackend/assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('blackend/assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('blackend/assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('blackend/assets/js/off-canvas.js')}}"></script>
+    <!-- endinject -->
+  </body>
+</html>
