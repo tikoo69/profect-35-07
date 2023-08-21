67a2d9e72d900b6dd60c6d3077a87908b9515065
Nakarin11 <NakarinChanthao36@gmail.com>
2023-08-21 10:26:16 +0700

    frontend init
    เพิ่มหน้า frontend

---
+++ resources/views/layouts/master_frontend.blade.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Coffee 248</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="assets/img/logo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Delicious
+  * Updated: Jul 27 2023 with Bootstrap v5.3.1
+  * Template URL: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center fixed-top topbar-transparent">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-center justify-content-lg-start">
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+       <img align="left" src="assets/img/logo.png" alt="" width="100" height="100" />"
+
+
+      <div class="logo me-auto">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+      <nav id="navbar" class="navbar order-last order-lg-0">
+
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">หน้าแรก</a></li>
+          <li><a class="nav-link scrollto" href="#about">แนะนำ</a></li>
+          <li><a class="nav-link scrollto" href="#menu">เมนู</a></li>
+          <li><a class="nav-link scrollto" href="#contact">ติดต่อ</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#book-a-table" class="book-a-table-btn scrollto">เข้าสู่ระบบ</a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/slide/หน้าร้าน.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown"><span>Coffee 248 </span> Cafe</h2>
+                <p class="animate__animated animate__fadeInUp"></p>
+                <div>
+                  <a href="#menu" class="btn-menu animate__animated animate__fadeInUp scrollto">Menu</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container-fluid">
+
+        <div class="row">
+
+          <div class="col-lg-5 align-items-stretch video-box" style='background-image: url("assets/img/about.jpg");'>
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+          </div>
+
+          <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch">
+
+            <div class="content">
+              <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
+              <p>
+
+              </p>
+              <p class="fst-italic">
+
+              </p>
+              <ul>
+                <li><i class=""></i> </li>
+                <li><i class=""></i> </li>
+                <li><i class=""></i> </li>
+              </ul>
+              <p>
+
+              </p>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Menu Section ======= -->
+    <section id="menu" class="menu">
+      <div class="container">
+
+        <div class="section-title">
+          <h2><span>Menu</span></h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="menu-flters">
+              <li data-filter="*" class="filter-active">ทั้งหมด</li>
+              <li data-filter=".filter-tea">ชา</li>
+              <li data-filter=".filter-coffee">กาแฟ</li>
+              <li data-filter=".filter-sweet">น้ำหวาน</li>
+              <li data-filter=".filter-other">อื่นๆ</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row menu-container">
+
+          <div class="col-lg-4 menu-item filter-sweet">
+            <div class="menu-content">
+             <a href="#">โกโก้</a> <span>45 บาท</span>
+              <img src="assets/img/menu/โกโก้เย็น.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-tea">
+            <div class="menu-content">
+             <a href="#">ชาเขียว</a> <span>45 บาท</span>
+              <img src="assets/img/menu/ชาเขียว.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-tea">
+            <div class="menu-content">
+             <a href="#">ชาเย็น</a> <span>45 บาท</span>
+              <img src="assets/img/menu/ชาเย็น.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-coffee">
+            <div class="menu-content">
+             <a href="#">ลาเต้</a> <span>45 บาท</span>
+              <img src="assets/img/menu/ลาเต้.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-coffee">
+            <div class="menu-content">
+             <a href="#">อเมริกาโน่</a> <span>45 บาท</span>
+              <img src="assets/img/menu/อเมริกาโน่.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-coffee">
+            <div class="menu-content">
+             <a href="#">เอสเพรสโซ่</a> <span>45 บาท</span>
+              <img src="assets/img/menu/เอสเพรสโซ่เย็น.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-sweet">
+            <div class="menu-content">
+             <a href="#">โอวัลตินเย็น</a> <span>30 บาท</span>
+              <img src="assets/img/menu/โอวัลตินเย็น.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-tea">
+            <div class="menu-content">
+             <a href="#">ชาเขียวมะนาว</a> <span>40 บาท</span>
+              <img src="assets/img/menu/ชาเขียวมะนาว.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-tea">
+            <div class="menu-content">
+             <a href="#">ชามะนาวสด</a> <span>40 บาท</span>
+              <img src="assets/img/menu/ชามะนาวสด.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-other">
+            <div class="menu-content">
+             <a href="#">น้ำผึ้งมะนาว</a> <span>40 บาท</span>
+              <img src="assets/img/menu/น้ำผึ้งมะนาว.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-other">
+            <div class="menu-content">
+             <a href="#">น้ำแดง</a> <span>25 บาท</span>
+              <img src="assets/img/menu/น้ำแดง.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+          <div class="col-lg-4 menu-item filter-other">
+            <div class="menu-content">
+             <a href="#">น้ำเขียว</a> <span>25 บาท</span>
+              <img src="assets/img/menu/น้ำเขียว.png" class="" alt="" width="400" height="300">
+            </div>
+            <div class="menu-ingredients">
+            </div>
+          </div>
+
+    </section><!-- End Menu Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2><span>Contact</span> Us</h2>
+          <p></p>
+        </div>
+      </div>
+
+      <div class="map">
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3257.857541735066!2d100.43398211019891!3d13.822977285588998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e291e2c9c07303%3A0x2b21361fce70ab7a!2z4LiV4Lil4Liy4LiU4Lie4Lij4Liw4Lij4Liy4LihIDU!5e0!3m2!1sth!2sth!4v1691231615250!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <div class="container mt-5">
+
+        <div class="info-wrap">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 info">
+              <i class="bi bi-geo-alt"></i>
+              <h4>ที่อยู่</h4>
+              <p>ตลาดพระราม 5 ถ.นครอินทร์ ต.บางสีทอง
+                อ.เมืองนนทบุรี จ.นนทบุรี 11130<br></p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+              <i class="bi bi-clock"></i>
+              <h4>เวลา เปิด - ปิด</h4>
+              <p>จันทร์ - เสาร์:<br>7:30 - 17:00</p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+              <i class="bi bi-facebook"></i>
+              <h4>Facebook</h4>
+              <p>Coffee248ตลาดพระรามห้า<br></p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+              <i class="bi bi-phone"></i>
+              <h4>เบอร์โทร</h4>
+              <p>080-1649492</p>
+            </div>
+          </div>
+        </div>
+
+        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+          <div class="row">
+            <div class="col-md-6 form-group">
+              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+            </div>
+            <div class="col-md-6 form-group mt-3 mt-md-0">
+              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+            </div>
+          </div>
+          <div class="form-group mt-3">
+            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+          </div>
+          <div class="form-group mt-3">
+            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+          </div>
+          <div class="my-3">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your message has been sent. Thank you!</div>
+          </div>
+          <div class="text-center"><button type="submit">Send Message</button></div>
+        </form>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Delicious</h3>
+      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>Delicious</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+
